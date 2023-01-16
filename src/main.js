ef8fca47390eb7b63df3d6c6bbcdae76673d43e5
blueqwertz <timlimlei@icloud.com>
2023-01-16 15:29:51 +0100

    changes in ui

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 const resize_fn = () => {
     document.getElementById("squares").style = ""
-    let _size = Math.min(document.getElementById("field").offsetHeight, document.getElementById("field").offsetWidth)
+    let _size = Math.min(Math.min(document.getElementById("field").offsetHeight, document.getElementById("field").offsetWidth) - 30, document.body.offsetHeight * 0.8)
     document.getElementById("squares").style.width = _size + "px"
     document.getElementById("squares").style.height = _size + "px"
     console.log(_size)
